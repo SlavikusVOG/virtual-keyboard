ee9bde0c4d20ddc8a91ae292ee75c75fc76912fb
Viacheslav Olishevko <volishevko@xbsoftware.by>
2022-05-06 01:42:00 +0300

    feat: add keyboard layout keys

--- src/view/keyboard/keyboard.js
+++ src/view/keyboard/keyboard.js
@@ -1,5 +1,15 @@
+import constants from '../../const';
+
 export default class Keyboard {
   constructor() {
     this.keyboard = document.createElement('div');
+    for (let rowIndex = 0; rowIndex < constants.EN_US_LAYOUT.length; rowIndex++) {
+      const row = this.keyboard.appendChild(document.createElement('div'));
+      for(let buttonIndex = 0; buttonIndex < constants.EN_US_LAYOUT[rowIndex].length; buttonIndex++) {
+        const newButton = row.appendChild(document.createElement('div'));
+        const eng = newButton.appendChild(document.createElement('span'));
+        const caseDown = eng
+      }
+    }
   }
 }
